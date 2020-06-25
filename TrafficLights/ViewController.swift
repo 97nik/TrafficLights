c001e260f28e4f1edf038d857d8d6eb73e4e2e9e
Nikita <57098717+97nik@users.noreply.github.com>
2020-06-25 15:05:13 +0300

    практически все сделал, осталось закруглить сигналы светофора

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet weak var red: UIView!
+    @IBOutlet weak var yellow: UIView!
+    @IBOutlet weak var green: UIView!
+    @IBOutlet weak var showButton: UIButton!
+    
+    private let bright = 1
+    private let pale = 0.1
+    private var step = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        red.alpha = CGFloat(pale)
+        yellow.alpha = CGFloat(pale)
+        green.alpha = CGFloat(pale)
+        showButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func ShowButtonPress() {
+        showButton.setTitle("Next", for: .normal)
 
+        if step == 1 {
+            green.alpha = CGFloat(pale)
+            red.alpha = CGFloat(bright)
+            step = 2
+    }
+        else if step == 2 {
+            yellow.alpha = CGFloat(bright)
+            red.alpha = CGFloat(pale)
+                 step = 3
+         }
+        else if step == 3 {
+            green.alpha = CGFloat(bright)
+            yellow.alpha = CGFloat(pale)
+                step = 1
+        }
+    }
+    
 }
 
